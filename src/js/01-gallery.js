790f1ee4b0e26e8a8b181a55baf5ac34dfc38611
Nikolay-Kovalov <lawyerpartners.ua@gmail.com>
2022-11-08 11:50:06 +0200

    Js commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
 
+
+const galleryRef = document.querySelector('.gallery')
+
+
+const markup = galleryItems.map(({ preview, original, description }) =>
+ `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>
+</div>`).join('');
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+
+new SimpleLightbox('.gallery__item', { captionsData: 'alt', captionDelay: 250 });
 console.log(galleryItems);
