732004542cdaf70399e4ef95fa7977019ab536e7
Nikolay-Kovalov <lawyerpartners.ua@gmail.com>
2022-11-10 14:35:00 +0200

    Edit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+    player.on('timeupdate',  throttle(onPlay, 1000));
+  function onPlay(data) {
+      localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds)); 
+    };
+
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
+
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from "lodash.throttle";
 
+const formRef = document.querySelector('.feedback-form');
+const inputRef = document.querySelector('input');
+const textareaRef = document.querySelector('textarea');
+
+const formData = {};
+
+const localStoregKey = "feedback-form-state";
+
+
+
+formRef.addEventListener('input', throttle (addToLocalStorage, 500));
+formRef.addEventListener('submit', onSubmit)
+addInput()
+
+function addToLocalStorage(evt) {
+  formData[evt.target.name] = evt.target.value
+ const obj = localStorage.setItem(localStoregKey, JSON.stringify(formData))
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  if (localStorage.getItem(localStoregKey)) {
+    console.log(JSON.parse(localStorage.getItem(localStoregKey)))
+  }
+  evt.currentTarget.reset();
+  localStorage.removeItem(localStoregKey);
+}
+
+function addInput() {
+  const saveData = localStorage.getItem(localStoregKey);
+  const parseData = JSON.parse(saveData);
+
+  if (parseData) {
+    inputRef.value = parseData.email || '';
+    textareaRef.value = parseData.message || '';
+  }
+}
